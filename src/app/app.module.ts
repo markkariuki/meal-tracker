c42d76b7b5a966f10ee0348360e6028019645042
david mark <markkariuki17@gmail.com>
2017-11-17 12:57:34 +0300

    add the meal-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MealListComponent } from './meal-list/meal-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MealListComponent
   ],
   imports: [
     BrowserModule
