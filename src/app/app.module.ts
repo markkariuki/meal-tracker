7ba07a854d580885bc6e7b80f980c035e20236af
david mark <markkariuki17@gmail.com>
2017-11-17 17:32:00 +0300

    add chnage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MealsComponent } from './meals/meals.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    MealsComponent,
   ],
 
   imports: [
