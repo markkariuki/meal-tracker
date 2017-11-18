84c074eb99594932a317f14d1cdd2c673bb4d88e
david mark <markkariuki17@gmail.com>
2017-11-18 09:54:07 +0300

    add files and stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'meal-tracker';
+
+  public tasks: Task[] = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+
+  ];
 }
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {   }
+}
+  
