644cf7f4ca1c77b05dbf7555cb5e64b32cae3792
david mark <markkariuki17@gmail.com>
2017-11-21 11:35:46 +0300

    add change to all the components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,22 @@
 import { Component } from '@angular/core';
-
+import { Task } from './meal.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'meal-tracker';
-  public tasks: Task[] = [
-        new Task("Create To-Do List app.", 0),
-        new Task("Learn Kung Fu.", 1),
-        new Task("Rewatch all the Lord of the Rings movies.", 2),
-        new Task("Do the laundry.", 3)
-    ];
-    selectedTask: Task = this.tasks[0];
+  public masterTaskList: Task[] = [
+       new Task("Create To-Do List app.", 0),
+       new Task("Learn Kung Fu.", 1),
+       new Task("Rewatch all the Lord of the Rings movies.", 2),
+       new Task("Do the laundry.", 3)
+   ];
+   selectedTask: Task = null;
    showDetails(clickedTask: Task) {
      this.selectedTask = clickedTask;
    }
    finishedEditing() {
      this.selectedTask = null;
    }
-}
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public id: number) {   }
-}
+ }
--- src/app/meal-lists/meal-lists.component.ts
+++ src/app/meal-lists/meal-lists.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../meal.model';
 
 @Component({
   selector: 'app-meal-lists',
@@ -6,10 +7,5 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meal-lists.component.css']
 })
 export class MealListsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+    @Input() childTaskList: Task[];
   }
-
-}
