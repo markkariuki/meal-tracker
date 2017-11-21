99d1d6a5b2782471be297670c08d81863bc4a7d5
david mark <markkariuki17@gmail.com>
2017-11-21 10:36:37 +0300

    show the different tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'meal-tracker';
-  public task: Task = new Task("Create To-Do List app.", 0);
+  public tasks: Task[] = [
+        new Task("Create To-Do List app.", 0),
+        new Task("Learn Kung Fu.", 1),
+        new Task("Rewatch all the Lord of the Rings movies.", 2),
+        new Task("Do the laundry.", 3)
+    ];
 
 }
 
